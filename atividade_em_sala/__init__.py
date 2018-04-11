804028cd669cf5f7209da283049ee64ab0db05fc
Ellen de Almeida <ellencosta23@gmail.com>
2018-04-11 18:00:26 -0300

    A imbecil esqueceu de importar o numpy e o matplotlib

--- atividade_em_sala/__init__.py
+++ atividade_em_sala/__init__.py
@@ -1,17 +1,23 @@
+import matplotlib as plt
+import numpy as np
+
 #Constantes utilizadas
 
 h = 6.626e-34
 c = 3.0e+8
 k = 1.38e-23
 wav = np.arange(1e-9, 3e-6, 1e-9)
+
+#Definindo a funcao
+
 def planck(T):
-	'''Esta funcao apresenta como saida o grafico da curva de emissao de um corpo negro de temperatura T. 
+    '''Esta funcao apresenta como saida o grafico da curva de emissao de um corpo negro de temperatura T. 
 	INPUT: Temperatura em Kelvin
 	OUTPUT: curva de corpo negro Intensidade[unidade indefinida] vs. comprimento de onda [nm]
 	'''
-    a = 2.0*h*c**2
-    b = h*c/(wav*k*T)
+    a=2.0*h*c*c
+    b=h*c/(wav*k*T)
     intensity = a/(wav*(np.exp(b)-1.0))
     
-    plot(wav,intensity,'g-')
-    show()
+    plt.plot(wav,intensity,'g-')
+    plt.show()
